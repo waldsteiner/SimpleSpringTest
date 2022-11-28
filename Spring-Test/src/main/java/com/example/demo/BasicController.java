54a9b2434eaa38a958d5eef95e159bfa4c312d79
waldsteiner <waldsteiner@gmail.com>
2022-11-28 23:46:32 +0900

    test #8

--- Spring-Test/src/main/java/com/example/demo/BasicController.java
+++ Spring-Test/src/main/java/com/example/demo/BasicController.java
@@ -94,4 +94,9 @@ public class BasicController {
 
         return "basic/operation";
     }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
 }
